0be8351e8b7ec7bded00ace6a8d0b7f9f54f788d
plz717 <panglinzhuo@gmail.com>
2017-01-16 10:53:51 +0800

    modify image DIR

--- plz-cae/enough/plz_CAE.py
+++ plz-cae/enough/plz_CAE.py
@@ -2,13 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import math
 import os
-import sys
-import tempfile
-import time
 import numpy as np
-import PIL
 from PIL import Image
 import tensorflow as tf
 from my_image_dataset import image_dataset
@@ -16,9 +11,9 @@ import pickle
 
 
 def convert_to_array_queue(files, files_num, height, width, channels):
-        # convert files containing multple images in png format to
-   # gray_scale images and pack them in
-   # image batch[batch_num,height,width]
+    # convert files containing multple images in png format to
+    # gray_scale images and pack them in
+    # image batch[batch_num,height,width]
 
     filenames = tf.train.match_filenames_once(files)
     filename_queue = tf.train.string_input_producer(filenames)
@@ -28,13 +23,13 @@ def convert_to_array_queue(files, files_num, height, width, channels):
     filename, content = reader.read(filename_queue)
     image = tf.image.decode_jpeg(content, channels=channels)
     # for python 3
-    #resized_image = tf.image.resize_images(image, [height, width])
+    # resized_image = tf.image.resize_images(image, [height, width])
     # for python 2
     resized_image = tf.image.resize_images(image, height, width)
     gray_images = tf.image.rgb_to_grayscale(resized_image)
 
     # step 4: batching
-    image_batch = tf.train.batch([gray_images], batch_size=1)
+    image_batch = tf.train.batch([gray_images], batch_sizre=1)
     batch_size = 1
     batch_num = int(files_num / batch_size)
 
@@ -52,32 +47,37 @@ def convert_to_array_queue(files, files_num, height, width, channels):
         # convert list to array
         image_total = np.array(image_total)  # (batch_num,height,W1idth,1))
         image_total = np.multiply(image_total, 1.0 / 255.0)
-        #image_total = np.reshape(image_total, (batch_num, height, W1idth))
+        # image_total = np.reshape(image_total, (batch_num, height, Width))
         num_examples = image_total.shape[0]
 
         coord.request_stop()
         coord.join(threads)
     return image_total, num_examples
 
+
+# train parameters
 learning_rate = 0.00001
 training_epochs = 10
-
+train_batch_size = 2
+# image patameters
+channels = 3  # original depth of input images
+DATA_SET_DIR = "/home/plz/slam/Images_12/"
+files_num = 12  # number of train dataset
 height = 480
 width = 640
-depth = 1
+depth = 1  # depth of train_dataset
+
+# convolution parameters
 filter_side = 3
 stride = 1
 filters_number = 32
 amount = filter_side - 1
 
-channels = 3  # original depth of input images
-files = "/home/plz/Images_12/*.jpg"
-files_num = 12
+files = DATA_SET_DIR + "*.jpg"
 
-train_batch_size = 2
 
-# Images. 4D tensor of[b1atch_size, self._image_height, self._image_W1idth,
-# self._image_depth] size
+# Images:4D tensor of[batch_size, height, width,
+# depth size
 images, num_examples = convert_to_array_queue(
     files, files_num, height, width, channels)
 
@@ -93,7 +93,7 @@ shape1 = [filter_side, filter_side, pad_input.get_shape()[3].value,
 W1 = tf.Variable(tf.random_normal(shape1), dtype=tf.float32)
 b1 = tf.Variable(tf.zeros([shape1[3]]), dtype=tf.float32)
 
-# decode layer
+# decode layer W&b
 shape2 = [filter_side, filter_side, filters_number, pad_input.get_shape()[
     3].value]
 W2 = tf.Variable(tf.random_normal(shape2), dtype=tf.float32)
@@ -146,29 +146,28 @@ with tf.Session() as sess:
     save_path = saver.save(sess, "CAE_model.ckpt")
     print("Model saved in file: ", save_path)
 
-    filenames = os.listdir("/home/plz/Images_12")
+    filenames = os.listdir(DATA_SET_DIR)
 
     features_list = []
     for image in filenames:
-        im_test = Image.open("/home/plz/Images_12/"+image)
+        im_test = Image.open(DATA_SET_DIR + image)
         im_test = im_test.convert("L")  # uint8
         im_test = np.reshape(im_test, (-1, height, width, 1))
         im_test = im_test.astype(np.float32)
 
         im_test = np.multiply(im_test, 1.0 / 255.0)
-        pad_im_test = tf.pad(im_test,
-                             [[0, 0], [amount, amount], [amount, amount], [0, 0]])
+        pad_im_test = tf.pad(
+            im_test, [[0, 0], [amount, amount], [amount, amount], [0, 0]])
 
         encode_result = tf.nn.bias_add(tf.nn.conv2d(
             pad_im_test, W1, [1, stride, stride, 1], 'VALID'), b1)
         encode = tf.nn.tanh(encode_result)
-        features_list.append(encode.eval())   #append an array to list
+        features_list.append(encode.eval())  # append an array to list
         # print(sess.run(encode))
-    #print(features_list)
-    #print("feature0 is: ",features_list[0].eval())
-    #print("feature0 shape is:",(features_list[0].eval()).shape)
-    
-    #dum features_array_list into a test.txt file
-    with open("test.txt","wb") as fp:
-        pickle.dump(features_list,fp)
+    # print(features_list)
+    # print("feature0 is: ",features_list[0].eval())
+    # print("feature0 shape is:",(features_list[0].eval()).shape)
 
+    # dum features_array_list into a test.txt file
+    with open("test.txt", "wb") as fp:
+        pickle.dump(features_list, fp)
